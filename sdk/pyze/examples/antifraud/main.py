bd3f9c3e9dc21226361194bb52745f1145381680
Pavel Pantiukhov <pantyukhov.p@gmail.com>
2023-08-19 14:49:53 +0200

    added localhost rippled

--- sdk/pyze/examples/antifraud/main.py
+++ sdk/pyze/examples/antifraud/main.py
@@ -2,13 +2,13 @@ import click
 # Define the network client
 from xrpl.clients import JsonRpcClient
 from xrpl.models import Payment
-from xrpl.transaction import submit_and_wait
+from xrpl.transaction import submit_and_wait, sign_and_submit
 from xrpl.utils import xrp_to_drops
 from xrpl.wallet import Wallet
 
 from pyze.memo import generate_verification_memos
 
-JSON_RPC_URL = "https://hooks-testnet-v3.xrpl-labs.com"
+JSON_RPC_URL =  "http://127.0.0.1:5005"
 client = JsonRpcClient(JSON_RPC_URL)
 
 
@@ -17,20 +17,21 @@ def get_ml_proof_memos():
 
 
 @click.command()
-@click.option('--wallet_address', default='raXixkeipodTmRv972HTKycS36PrEUwSzA',
+@click.option('--admin_seed', default='snoPBrXtMeMyMHUVTgbuqAfg1SUTb',
               help='Admin seed')  # seed 'sEdTE5SpNXuPc6h3oyGKaWfQXkyssPS'
-@click.option('--client_seed', default='sEdSciSwFeTbWv8JpxrUZSji2ahNrPH', help='Client seed')
-def send_payment(wallet_address, client_seed):
+@click.option('--client_seed', default='sawmDjPis6h6AS9g5XYmviJ6N6EKu', help='Client seed')
+def send_payment(admin_seed, client_seed):
     client_account = Wallet.from_seed(client_seed)
+    admin_account = Wallet.from_seed(admin_seed)
 
     my_tx_payment = Payment(
         account=client_account.classic_address,
         amount=xrp_to_drops(22),
-        destination=wallet_address,
-        memos=get_ml_proof_memos()
+        destination=admin_account.classic_address,
+        # memos=get_ml_proof_memos()
     )
 
-    tx_response = submit_and_wait(my_tx_payment, client, client_account)
+    tx_response = sign_and_submit(my_tx_payment, client, client_account)
     print(tx_response)
 
 
