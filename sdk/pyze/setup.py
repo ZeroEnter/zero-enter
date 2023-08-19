59b841d7d94dd2b6659bc518c04a3b8e460b966d
pavelpantukhov <pantyukhov.p@gmail.com>
2023-08-19 11:12:12 +0200

    added sdk for ezkl

---
+++ sdk/pyze/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyze',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=[''],
+    author='Zero Enter',
+    description='Zero Enter sdk',
+)
