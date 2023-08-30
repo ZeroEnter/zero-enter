0146dfee18cc6d4e64fcad95976627f8a65d7ed7
pavelpantukhov <pantyukhov.p@gmail.com>
2023-08-30 19:46:55 +0200

    minor fix

--- sdk/pyze/pyze/websocket/xrpl.py
+++ sdk/pyze/pyze/websocket/xrpl.py
@@ -1,5 +1,7 @@
+import json
 import traceback
 
+import requests
 from xrpl.clients import JsonRpcClient, WebsocketClient
 from xrpl.wallet import Wallet
 from xrpl.models import Subscribe, StreamParameter
@@ -15,8 +17,18 @@ class XRPLHooksListener:
         self.transfer_wallet = Wallet.from_seed("shpJBbnNVAgPbv7NLwvS3nUbeahR4")
         self.transfer_wallet_address = "rwY5SoDxyfUcUijRvFyvJLnGy5CYPU5amZ"
 
-    def prepare_transaction(self):
-        raise NotImplementedError
+    def prepare_transaction(self, message):
+        proof_link = ""
+        for memo in message['transaction']['Memos']:
+            memo_type = bytes.fromhex(memo['Memo']["MemoType"]).decode('utf-8')
+            memo_data = bytes.fromhex(memo['Memo']["MemoData"]).decode('utf-8')
+            if memo_type == 'proof-link':
+                proof_link = memo_data
+
+        response = requests.get(proof_link)
+        data = json.loads(response.content)
+
+        print(data)
 
     def listen(self):
         url = "wss://hooks-testnet-v3.xrpl-labs.com/"
@@ -28,6 +40,6 @@ class XRPLHooksListener:
                 print(message)
                 if message.get('transaction', {}).get('Destination', '') == self.transfer_wallet_address:
                     try:
-                        self.prepare_transaction()
+                        self.prepare_transaction(message)
                     except:
                         traceback.print_exc()
